78033d398213a21f1a52c46dabdc4c7a1d6d6345
MAZubaer <zubaerniloy142@gmail.com>
2026-04-19 05:37:07 +0600

    App name and Image Update change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Repairix') }}</title>
+    <link rel="icon" href="{{ asset('images/repairix_short.png') }}" type="image/png" />
+    <link rel="apple-touch-icon" href="{{ asset('images/repairix_short.png') }}" />
     @inertiaHead
     @vite(['resources/js/app.js', 'resources/css/app.css'])
   </head>
